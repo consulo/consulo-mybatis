f64bb41e3f3dc46cc241f043b1ea590583502fb5
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-10-14 18:35:40 +0400

    compile fix

--- src/main/java/net/ishchenko/idea/minibatis/inspections/mapper/MyBatisInspection.java
+++ src/main/java/net/ishchenko/idea/minibatis/inspections/mapper/MyBatisInspection.java
@@ -3,8 +3,7 @@ package net.ishchenko.idea.minibatis.inspections.mapper;
 import com.intellij.java.analysis.impl.codeInspection.BaseJavaLocalInspectionTool;
 import com.intellij.java.language.JavaLanguage;
 import consulo.language.Language;
-import org.jetbrains.annotations.Nls;
-
+import consulo.localize.LocalizeValue;
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
 
@@ -14,27 +13,22 @@ import jakarta.annotation.Nullable;
  * Date: 02.06.12
  * Time: 11:47
  */
-public abstract class MyBatisInspection extends BaseJavaLocalInspectionTool
-{
-	@Nullable
-	@Override
-	public Language getLanguage()
-	{
-		return JavaLanguage.INSTANCE;
-	}
+public abstract class MyBatisInspection extends BaseJavaLocalInspectionTool {
+    @Nullable
+    @Override
+    public Language getLanguage() {
+        return JavaLanguage.INSTANCE;
+    }
 
-	@Nls
-	@Nonnull
-	@Override
-	public String getGroupDisplayName()
-	{
-		return "iBATIS/MyBatis issues";
-	}
+    @Nonnull
+    @Override
+    public LocalizeValue getGroupDisplayName() {
+        return LocalizeValue.localizeTODO("iBATIS/MyBatis issues");
+    }
 
-	@Nonnull
-	@Override
-	public String getShortName()
-	{
-		return this.getClass().getName();
-	}
+    @Nonnull
+    @Override
+    public String getShortName() {
+        return this.getClass().getSimpleName();
+    }
 }
--- src/main/java/net/ishchenko/idea/minibatis/inspections/mapper/NoMapperStatementInspection.java
+++ src/main/java/net/ishchenko/idea/minibatis/inspections/mapper/NoMapperStatementInspection.java
@@ -10,10 +10,10 @@ import consulo.language.editor.inspection.LocalQuickFix;
 import consulo.language.editor.inspection.ProblemDescriptor;
 import consulo.language.editor.inspection.ProblemHighlightType;
 import consulo.language.editor.inspection.scheme.InspectionManager;
-import net.ishchenko.idea.minibatis.DomFileElementsFinder;
-import org.jetbrains.annotations.Nls;
-
+import consulo.localize.LocalizeValue;
 import jakarta.annotation.Nonnull;
+import net.ishchenko.idea.minibatis.DomFileElementsFinder;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Max
@@ -21,46 +21,38 @@ import jakarta.annotation.Nonnull;
  * Time: 11:39
  */
 @ExtensionImpl
-public class NoMapperStatementInspection extends MyBatisInspection
-{
-	@Override
-	public ProblemDescriptor[] checkMethod(@Nonnull PsiMethod method, @Nonnull InspectionManager manager, boolean isOnTheFly, Object state)
-	{
-		PsiClass containingClass = method.getContainingClass();
-		if(containingClass != null && containingClass.isInterface())
-		{
+public class NoMapperStatementInspection extends MyBatisInspection {
+    @Override
+    public ProblemDescriptor[] checkMethod(@Nonnull PsiMethod method, @Nonnull InspectionManager manager, boolean isOnTheFly, Object state) {
+        PsiClass containingClass = method.getContainingClass();
+        if (containingClass != null && containingClass.isInterface()) {
 
-			PsiIdentifier methodName = method.getNameIdentifier();
-			DomFileElementsFinder finder = ServiceManager.getService(containingClass.getProject(), DomFileElementsFinder.class);
-			boolean existsMapperStatement = finder.existsMapperStatement(method);
+            PsiIdentifier methodName = method.getNameIdentifier();
+            DomFileElementsFinder finder = ServiceManager.getService(containingClass.getProject(), DomFileElementsFinder.class);
+            boolean existsMapperStatement = finder.existsMapperStatement(method);
 
-			boolean containsIbatisAnnotation = false;
-			for(PsiAnnotation annotation : method.getModifierList().getAnnotations())
-			{
-				String annotationName = annotation.getQualifiedName();
-				if(annotationName != null && annotationName.startsWith("org.apache.ibatis.annotations"))
-				{
-					containsIbatisAnnotation = true;
-					break;
-				}
-			}
+            boolean containsIbatisAnnotation = false;
+            for (PsiAnnotation annotation : method.getModifierList().getAnnotations()) {
+                String annotationName = annotation.getQualifiedName();
+                if (annotationName != null && annotationName.startsWith("org.apache.ibatis.annotations")) {
+                    containsIbatisAnnotation = true;
+                    break;
+                }
+            }
 
-			if(!existsMapperStatement && !containsIbatisAnnotation && methodName != null)
-			{
-				return new ProblemDescriptor[]{
-						manager.createProblemDescriptor(methodName, "Statement with id=\"#ref\" not defined in mapper xml", (LocalQuickFix) null, ProblemHighlightType.GENERIC_ERROR, isOnTheFly)
-				};
-			}
-		}
-		return null;
-	}
+            if (!existsMapperStatement && !containsIbatisAnnotation && methodName != null) {
+                return new ProblemDescriptor[]{
+                    manager.createProblemDescriptor(methodName, "Statement with id=\"#ref\" not defined in mapper xml", (LocalQuickFix) null, ProblemHighlightType.GENERIC_ERROR, isOnTheFly)
+                };
+            }
+        }
+        return null;
+    }
 
-	@Nls
-	@Nonnull
-	@Override
-	public String getDisplayName()
-	{
-		return "Mapper interface method not defined in xml";
-	}
+    @Nonnull
+    @Override
+    public LocalizeValue getDisplayName() {
+        return LocalizeValue.localizeTODO("Mapper interface method not defined in xml");
+    }
 
 }
